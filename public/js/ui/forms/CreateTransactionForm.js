4e6e09932eb34ad074ace4a436211685e19779a8
workplace <semenenko.sse@mail.ru>
2025-08-15 09:23:49 +0300

    10 commit

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -8,7 +8,9 @@ class CreateTransactionForm extends AsyncForm {
    * метод renderAccountsList
    * */
   constructor(element) {
-    super(element)
+    super(element);
+    if (!element) throw new Error('CreateTransactionForm: element is required');
+    this.element = element;
     this.renderAccountsList();
   }
 
@@ -17,14 +19,17 @@ class CreateTransactionForm extends AsyncForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
-    Account.list(null, (error, response) => {
-      if (response && response.success && response.data.length) {
-        const accountsSelect = this.element.getElementsByClassName('accounts-select')[0];
-        accountsSelect.innerHTML = '';
-        for (let item of response.data) {
-          accountsSelect.insertAdjacentHTML('afterbegin', `<option value="${item.id}">${item.name}</option>`);
-        }
-      }
+    // Найти все select-ы для аккаунтов в форме (в проекте два экземпляра формы)
+    const selects = this.element.querySelectorAll('select.accounts-select');
+    // Очистка и загрузка списка через Account.list
+    Account.list(null, (err, response) => {
+      if (!response || !response.success) return;
+      const accounts = response.data || [];
+      // Формируем HTML для опций
+      const optionsHtml = accounts.map(acc => `<option value="${acc.id}">${acc.name}</option>`).join('');
+      selects.forEach(sel => {
+        sel.innerHTML = optionsHtml;
+      });
     });
   }
 
@@ -35,12 +40,18 @@ class CreateTransactionForm extends AsyncForm {
    * в котором находится форма
    * */
   onSubmit(data) {
-    Transaction.create(data, (error, response) => {
-      if (response && response.success) {
+    // data приходит как объект полей формы (AsyncForm обеспечивает это)
+    Transaction.create(data, (err, response) => {
+      if (!response) return;
+      if (response.success) {
+        // Закрыть модальное окно, очистить форму, обновить приложение
+        const modal = App.getModal(this.element.closest('.modal')?.dataset?.modalId) || App.getModal('createTransaction');
+        if (modal) modal.close();
         this.element.reset();
-        App.update();
-        const modal = App.getModal(`new${data.type.charAt(0).toUpperCase() + data.type.slice(1)}`);
-        modal.close();
+        App.update(); // обновить виджеты/балансы/список счетов, как требуется в проекте
+      } else {
+        // Обработка ошибки (можно показать сообщение)
+        console.error('Transaction.create error', response.error);
       }
     });
   }
